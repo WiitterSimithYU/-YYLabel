a18a07218e9487f7809911c9e1e8d39807ac7ab9
WiitterSimithYU <378961614@qq.com>
2018-05-02 14:14:40 +0800

    commit

---
+++ 富文本YYLabel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  富文本YYLabel
+//
+//  Created by miaomiaokeji on 2018/5/2.
+//  Copyright © 2018年 weiliams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 富文本YYLabel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  富文本YYLabel
+//
+//  Created by miaomiaokeji on 2018/5/2.
+//  Copyright © 2018年 weiliams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
